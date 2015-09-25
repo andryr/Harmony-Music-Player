f8ba235042b3feb03af02c47434f7da61edb6cf3
Andry Rafaralahy <arafaralahy@gmail.com>
2015-09-25 20:11:09 +0200

    ajout d'une activité "À propos"

---
+++ app/src/main/java/com/andryr/musicplayer/preferences/AboutActivity.java
@@ -0,0 +1,42 @@
+package com.andryr.musicplayer.preferences;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+import com.andryr.musicplayer.BaseActivity;
+import com.andryr.musicplayer.R;
+
+public class AboutActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        WebView licensesView = (WebView) findViewById(R.id.licenses_view);
+        licensesView.loadUrl("file:///android_asset/licenses.html");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_about, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
