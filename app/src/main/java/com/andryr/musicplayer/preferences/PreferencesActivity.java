5a3fc19f6677ba942ea6489e7b6367152053afec
Andry Rafaralahy <arafaralahy@gmail.com>
2015-10-10 19:37:19 +0200

    prise en compte automatique des changements de thème

--- app/src/main/java/com/andryr/musicplayer/preferences/PreferencesActivity.java
+++ app/src/main/java/com/andryr/musicplayer/preferences/PreferencesActivity.java
@@ -1,25 +1,42 @@
 package com.andryr.musicplayer.preferences;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.NavUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.andryr.musicplayer.R;
-import com.andryr.musicplayer.preferences.AppCompatPreferenceActivity;
 
-public class PreferencesActivity extends AppCompatPreferenceActivity {
+public class PreferencesActivity extends AppCompatPreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     public static final String KEY_PREF_THEME_BASE = "pref_theme_base";
     public static final String KEY_PREF_THEME = "pref_theme";
+
+    private boolean mShouldRestart = false;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
 
 
     @Override
@@ -32,8 +49,7 @@ public class PreferencesActivity extends AppCompatPreferenceActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch(item.getItemId())
-        {
+        switch (item.getItemId()) {
             case android.R.id.home:
                 NavUtils.navigateUpFromSameTask(this);
                 return true;
@@ -43,6 +59,24 @@ public class PreferencesActivity extends AppCompatPreferenceActivity {
     }
 
 
+    @Override
+    public void onBackPressed() {
+        if (mShouldRestart) {
+            Intent i = getBaseContext().getPackageManager()
+                    .getLaunchIntentForPackage(getBaseContext().getPackageName());
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(KEY_PREF_THEME) || key.equals(KEY_PREF_THEME_BASE)) {
+            mShouldRestart = true;
+        }
+    }
 
 
 }
