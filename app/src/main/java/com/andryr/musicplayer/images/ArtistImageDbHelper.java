bc3b5bd7bf6ad233008ba1104f2acd0f552609cc
andry <arafaralahy@gmail.com>
2016-03-18 17:33:30 +0100

    changed DATABASE_VERSION to force database recreation

--- app/src/main/java/com/andryr/musicplayer/images/ArtistImageDbHelper.java
+++ app/src/main/java/com/andryr/musicplayer/images/ArtistImageDbHelper.java
@@ -31,7 +31,7 @@ import java.io.ByteArrayOutputStream;
  */
 public class ArtistImageDbHelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
     public static final String DATABASE_NAME = "ArtistImage.db";
 
     private static final String COMMA_SEP = ",";
