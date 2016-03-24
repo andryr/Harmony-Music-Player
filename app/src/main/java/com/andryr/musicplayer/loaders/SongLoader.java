3aa803a36fda18a24ed64fb62c7acc2cf080ad64
andry <arafaralahy@gmail.com>
2016-03-24 18:40:47 +0100

    only load music files

--- app/src/main/java/com/andryr/musicplayer/loaders/SongLoader.java
+++ app/src/main/java/com/andryr/musicplayer/loaders/SongLoader.java
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.v4.database.DatabaseUtilsCompat;
 import android.util.Log;
 
 import com.andryr.musicplayer.model.Song;
@@ -121,6 +122,8 @@ public class SongLoader extends BaseLoader<List<Song>> {
 
         String selection = getSelectionString();
         String[] selectionArgs = getSelectionArgs();
+
+        selection = DatabaseUtilsCompat.concatenateWhere(selection, MediaStore.Audio.Media.IS_MUSIC+" = 1");
 
         String fieldName = MediaStore.Audio.Media.TITLE;
         String filter = getFilter();
